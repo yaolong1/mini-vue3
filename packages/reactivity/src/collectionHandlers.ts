dbd3c31bd9b7090cae035a7d8187db7e0d13af5a
耀龙啊 <1638538651@qq.com>
2022-02-28 15:24:46 +0800

    feat: 避免set map响应式数据污染

--- packages/reactivity/src/collectionHandlers.ts
+++ packages/reactivity/src/collectionHandlers.ts
@@ -1,28 +1,38 @@
+import { hasChanged } from '@mini-vue3/shared';
 import { hasOwn } from '@mini-vue3/shared';
 import { ITERATE_KEY } from './baseHandlers';
 import { track, trigger } from './effect';
 import { TrackOpTypes, TriggerOpTypes } from './operators';
-import { ReactiveFlags, toRaw } from './reactive';
+import { ReactiveFlags, toRaw, toReactive } from './reactive';
 
 // 集合对象的拦截处理
 
-
+/**
+ * set集合的Add操作
+ * @param key 
+ * @returns 
+ */
 function add(key) {
   // 此时的this是指向的代理对象，所以直接访问代理对象获取target
   const target = toRaw(this)
   const hadKey = target.has(key)
   //如果当前要添加的值存在Set中就不需要触发更新
   if (!hadKey) {
-    const rawKey = key[ReactiveFlags.RAW] || key
+    const rawKey = toRaw(key) // 如果key是一个响应式对象直接转换为原型
     target.add(rawKey)
     trigger(target, TriggerOpTypes.ADD, rawKey)
   }
   return this
 }
 
+/**
+ * set的删除操作
+ * @param key 
+ * @returns 
+ */
 function deleteEntry(key) {
   const target = toRaw(this)
-  const hadKey = target.has(key)
+  const hadKey = target.has(key) //是否存在当前key
   let res = false //默认删除失败
   if (hadKey) {
     res = target.delete(key)
@@ -32,10 +42,70 @@ function deleteEntry(key) {
   return res
 }
 
+/**
+ * map获取操作
+ * @param key map的key
+ */
+function get(key) {
+  const target = toRaw(this) //原对象
+  const had = target.has(key)
+
+  track(target, TrackOpTypes.GET, key)
+  let res
+  if (had) {
+    res = target.get(key)
+    return toReactive(res) // 深度响应
+  }
+}
+
+/**
+ * map设置操作
+ * @param key 
+ * @param value 
+ */
+function set(key, value) {
+  const target = toRaw(this) //拿到原对象
+
+
+  /**
+   * 响应式数据设置到原对象上 这种情况就是数据污染。所以要把响应式对象变成原对象
+   * 
+   * 例子
+   * const m = new Map()
+   * 
+   * const a = reactive({a:1})
+   * const data = reactive(m)
+   * 
+   * data.set('key',a) // data=Proxy { 'key' => Proxy}   m={ 'key' => Proxy}
+   * 
+   * m.get('key').a //通过原对象拿到了响应式对象 ---响应式数据设置到原对象上了这种情况叫做数据污染
+   * 
+   **/
+  value = toRaw(value)
+
+
+  const oldValue = target.get(key)
+
+  const had = target.has(key)
+  target.set(key, value)
+  if (!had) {
+    // 新增
+    console.log('新增')
+    trigger(target, TriggerOpTypes.ADD, value)
+  } else if (hasChanged(value, oldValue)) {
+    // 更新
+    console.log('更新', '老对象', oldValue, '新对象', value)
+    trigger(target, TriggerOpTypes.SET, value, oldValue)
+  }
+  return this
+}
+
 function createInstrumentations() {
   const mutableInstrumentations = {
     add,
-    delete: deleteEntry
+    delete: deleteEntry,
+    set,
+    get
   }
 
 
@@ -93,7 +163,6 @@ function createInstrumentationsGetter(isReadonly = false, isShallow = false) {
     } else if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly
     }
-
 
 
     console.log('当前访问的key', key)
